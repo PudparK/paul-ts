b33cf0a65326968a37a165ba1acc0a1db1d982fe
Paul Barrón <podbarron@gmail.com>
2025-11-28 14:34:19 -0600

    Add resume PDF and update CV download button to point to new file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,11 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button
+        href="../docs/paul-barron-resume-2025.pdf"
+        variant="secondary"
+        className="group mt-6 w-full"
+      >
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
